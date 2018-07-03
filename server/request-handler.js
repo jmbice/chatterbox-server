f15b31a867fd48df14a5e3ac947eabd8e34afbbf
jmbice <jmbice@gmail.com>
2018-07-02 20:19:45 -0700

    dfdf

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,75 +1,90 @@
-/*************************************************************
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, Mr. Bae & Mr. Bice!');
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+var headers = defaultCorsHeaders;
+var data = {
+  results: [
+    // {
+    //   username: 'bob',
+    //   text: 'hi frank',
+    //   roomname: 'the interwebs',
+    //   createdAt: '2018-07-02T18:32:45.273Z',
+    //   updatedAt: '2018-07-02T18:32:45.273Z'
+    // },
+    // {
+    //   username: 'frank',
+    //   text: 'hi bob',
+    //   roomname: 'the interwebs',
+    //   createdAt: '2018-06-02T18:32:45.273Z',
+    //   updatedAt: '2018-06-02T18:32:45.273Z'
+    // },
+    // {
+    //   username: 'joe',
+    //   text: 'hi joe',
+    //   roomname: 'the interwebs',
+    //   createdAt: '2018-05-02T18:32:45.273Z',
+    //   updatedAt: '2018-05-02T18:32:45.273Z'
+    // }
+  ]
+};
+
+
+var requestHandler = function(request, response) {
+  var statusCode;
+
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  
+  if (request.method === "GET") {
+    headers['Content-Type'] = 'application/json';
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data));
+
+  } else if (request.method === "POST") {
+    headers['Content-Type'] = 'application/json';
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    data.results.push(request._postData); 
+    console.log(request) 
+    response.end(JSON.stringify(data));
+    // let body = [];
+    // request.on('data', (chunk) => {
+    //   body.push(chunk);
+      
+    // }).on('end', () => {
+    //   body = Buffer.concat(body).toString();
+    //   console.log(body);
+    
+    //   data.results.push(JSON.parse(body));
+    //   console.log(JSON.parse(body));
+    //   response.end(data);
+      
+    // })
+        
+    
+    
+    
+
+    
+  } else if (request.method === "OPTIONS") {
+    headers['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, headers);
+    response.end();
+    
+  } else {
+    console.log ('Error: ' + request.method + ' is not recognized.');
+  }
+   
+   
+  // response.writeHead(statusCode, headers);
+  // response.end('Hello, Mr. Bae & Mr. Bice!');
+  
+};
+
 
 exports.requestHandler = requestHandler;
-
